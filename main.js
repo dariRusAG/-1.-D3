3b428796cdf6186b1aea4ff3849791d4d4a6713c
dariRusAG <dariy.agapova@gmail.com>
2024-03-17 20:26:43 +1000

    new file:   classical_composers.csv 	new file:   composers.json 	new file:   index.html 	new file:   main.js

---
+++ main.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = '127.0.0.1';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+    // Обрабатываем запросы к файлам index.html и data.js
+    if (req.url === '/' || req.url === '/index.html') {
+        // Читаем и отправляем файл index.html
+        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+            if (err) {
+                res.writeHead(500);
+                res.end('Error loading index.html');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    } else if (req.url === '/composers.json') {
+        // Читаем и отправляем файл composers.json как JSON
+        fs.readFile(path.join(__dirname, '/composers.json'), (err, data) => {
+            if (err) {
+                res.writeHead(500);
+                res.end('Error loading composers.json');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(data);
+            }
+        });
+    } else {
+        // Обрабатываем другие запросы, например, запросы к другим ресурсам
+        res.writeHead(404);
+        res.end('Page not found');
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
